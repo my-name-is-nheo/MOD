080943b3e3b0e825496858d7c05354b586f1f245
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-19 13:13:54 -0400

    progress 2

--- db/recipe.sql
+++ db/recipe.sql
@@ -1,15 +1,16 @@
-CREATE DATABASE recipe_mvp
+CREATE DATABASE recipe_mvp;
 
-use recipe_mvp
+USE recipe_mvp;
 
 
 CREATE TABLE recipeList (
   id INTEGER AUTO_INCREMENT,
   food_name VARCHAR(200) NOT NULL,
-  description VARCHAR(300) NOT NULL,
-  ingredient_one VARCHAR (300) NOT NULL,
-  ingredient_two VARCHAR (300) NOT NULL,
-  ingredient_three VARCHAR (300),
-  ingredient_four VARCHAR (300),
-  ingredient_five VARCHAR (300),
-)
+  food_description VARCHAR(300) NOT NULL,
+  ingredient_one VARCHAR(300) NOT NULL,
+  ingredient_two VARCHAR(300) NOT NULL,
+  ingredient_three VARCHAR(300),
+  ingredient_four VARCHAR(300),
+  ingredient_five VARCHAR(300),
+  PRIMARY KEY (id)
+);
