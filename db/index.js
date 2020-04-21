3f00c129902a48482c10cd0afa7bfadce5dc5bc6
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-21 03:55:25 -0400

    heroku update

--- db/index.js
+++ db/index.js
@@ -4,12 +4,16 @@ const mysql = require("mysql");
 //   user: "root",
 //   database: "recipe_mvp",
 // });
-var dbConnection = mysql.createConnection({
-  host: "us-cdbr-iron-east-01.cleardb.net",
-  user: "b888a0a1cb3005",
-  password: "7fe08e8c",
-  database: "heroku_e073a55eda31fd0",
-});
+try {
+  var dbConnection = mysql.createConnection({
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "b888a0a1cb3005",
+    password: "7fe08e8c",
+    database: "heroku_e073a55eda31fd0",
+  });
+} catch (e) {
+  console.log("create connection error ", e);
+}
 
 dbConnection.connect();
 module.exports = dbConnection;
