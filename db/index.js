a5810d86a73195c0677c16427c5a07a01264745e
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-21 13:36:12 -0400

    heroku update

--- db/index.js
+++ db/index.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql");
 
-var dbConnection = mysql.createConnection({
-  user: "root",
-  database: "recipe_mvp",
-});
-// try {
-//   var dbPool = mysql.createPool({
-//     connectionLimit: 10,
-//     host: "us-cdbr-iron-east-01.cleardb.net",
-//     user: "b039ba6d119e9c",
-//     password: "4177f229",
-//     database: "heroku_2341fb153f62c79",
-//   });
-// } catch (e) {
-//   console.log("create connection error ", e);
-// }
+// var dbConnection = mysql.createConnection({
+//   user: "root",
+//   database: "recipe_mvp",
+// });
+try {
+  var dbPool = mysql.createPool({
+    connectionLimit: 10,
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "b039ba6d119e9c",
+    password: "4177f229",
+    database: "heroku_2341fb153f62c79",
+  });
+} catch (e) {
+  console.log("create connection error ", e);
+}
 
-dbConnection.connect();
-// module.exports = dbPool;
-module.exports = dbConnection;
+// dbConnection.connect();
+module.exports = dbPool;
+// module.exports = dbConnection;
