a150a39c1c0d8d83d55f97ff510bcd810399618d
Nicholas Heo <nicholas.heo95@gmail.com>
2020-04-21 13:23:14 -0400

    heroku new

--- db/index.js
+++ db/index.js
@@ -8,9 +8,9 @@ try {
   var dbPool = mysql.createPool({
     connectionLimit: 10,
     host: "us-cdbr-iron-east-01.cleardb.net",
-    user: "b888a0a1cb3005",
-    password: "7fe08e8c",
-    database: "heroku_e073a55eda31fd0",
+    user: "b039ba6d119e9c",
+    password: "4177f229",
+    database: "heroku_2341fb153f62c79",
   });
 } catch (e) {
   console.log("create connection error ", e);
